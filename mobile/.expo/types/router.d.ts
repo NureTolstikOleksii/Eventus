c9225ab07cb22b58798405721a3cbd6e05308feb
NureMiachKateryna <kateryna.miach@nure.ua>
2024-11-14 22:44:37 +0200

    Update router.d.ts

--- mobile/.expo/types/router.d.ts
+++ mobile/.expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/LoginScreen` | `/(tabs)/WelcomeScreen` | `/(tabs)/explore` | `/LoginScreen` | `/WelcomeScreen` | `/_sitemap` | `/explore` | `/types`;
+      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/WelcomeScreen` | `/(tabs)/explore` | `/WelcomeScreen` | `/_sitemap` | `/explore` | `/types`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
