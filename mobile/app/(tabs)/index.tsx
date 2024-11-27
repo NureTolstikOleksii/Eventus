e114a763fda9864bae6259937dee39e82f9e2935
NureMiachKateryna <kateryna.miach@nure.ua>
2024-11-27 17:30:59 +0200

    changed expo

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,20 +1,6 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import WelcomeScreen from '../(tabs)/WelcomeScreen';
-import UserProfile from '../(tabs)/UserProfile';
-import OrdersScreen from '../(tabs)/OrdersScreen'; // Импортируем OrdersScreen
-
-const Stack = createStackNavigator();
+import { Stack } from 'expo-router';
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="WelcomeScreen">
-        <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="UserProfile" component={UserProfile} />
-        <Stack.Screen name="OrdersScreen" component={OrdersScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <Stack />;
 }
