5dbec1af998f87e879123cf11896ff3f2237a0fe
NureMiachKateryna <kateryna.miach@nure.ua>
2024-11-28 23:46:05 +0200

    changed orders

--- mobile/app/(tabs)/Orders.tsx
+++ mobile/app/(tabs)/Orders.tsx
@@ -9,9 +9,7 @@ const ServiceScreen: React.FC = () => {
 
             <ScrollView contentContainerStyle={styles.scrollContainer}>
                 <View style={styles.header}>
-                    <TouchableOpacity>
-                        <Image source={require('../../assets/images/arrow.png')} style={styles.backIcon} />
-                    </TouchableOpacity>
+
                     <TouchableOpacity style={styles.backButton}>
                         <Image source={require('../../assets/images/backarrow.png')} style={styles.icon} />
                     </TouchableOpacity>
