1127b00590371c6421e8de98733f1798f973c368
Viktoriia Serdiuk <viktoriia.serdiuk@nure.ua>
2024-12-01 15:46:19 +0200

    changed ProfileCustomer

--- web/src/pages/ProfileCustomer.js
+++ web/src/pages/ProfileCustomer.js
@@ -2,16 +2,47 @@ import React, { useState } from "react";
 import "../css/ProfileCustomer.css";
 import profileFon from "../assets/profile-fon.png";
 import pencil from "../assets/pencil.png";
-import star from "../assets/star.png";
 import user from "../assets/user.png";
 import plus from "../assets/plus.png";
-import checkMark from "../assets/checkMark.png";
-import arrow from "../assets/arrow.png";
+import greenPlus from "../assets/green-plus.png";
+import greenArrow from "../assets/green-arrow.png";
 import flower from "../assets/flower.png";
-import serviceImage1 from "../assets/red-roses.jpg";
+import minus from "../assets/minus.png";
+import greenMinus from "../assets/green-minus.png";
 import { Link } from "react-router-dom";
 
 function ProfileCustomer() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [items, setItems] = useState([{ text: "", checked: false }]);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCheckboxChange = (index) => {
+    const newItems = [...items];
+    newItems[index].checked = !newItems[index].checked;
+    setItems(newItems);
+  };
+
+  const handleTextChange = (index, text) => {
+    const newItems = [...items];
+    newItems[index].text = text;
+    setItems(newItems);
+  };
+
+  const handleRemoveItem = (index) => {
+    const newItems = items.filter((_, i) => i !== index);
+    setItems(newItems);
+  };
+
+  const handleAddItem = () => {
+    setItems([...items, { text: "", checked: false }]);
+  };
   const list = [
     {
       name: "Букет",
@@ -41,10 +72,30 @@ function ProfileCustomer() {
   ];
 
   const orders = [
-    "Пакет послуг №1",
-    "Пакет послуг №2",
-    "Пакет послуг №3",
-    "Пакет послуг №4",
+    {
+      name: "Букет червоних троянд",
+      image: flower,
+      date: "01.12.2024",
+      price: "1000 грн",
+    },
+    {
+      name: "Букет лілій",
+      image: flower,
+      date: "02.12.2024",
+      price: "1200 грн",
+    },
+    {
+      name: "Букет орхідей",
+      image: flower,
+      date: "03.12.2024",
+      price: "1500 грн",
+    },
+    {
+      name: "Букет нареченої",
+      image: flower,
+      date: "04.12.2024",
+      price: "1800 грн",
+    },
   ];
 
   return (
@@ -75,7 +126,6 @@ function ProfileCustomer() {
 
         <div className="profile-customer-block">
           <div className="profile-customer-second-block-header">
-            <img src={pencil} alt="Edit" className="icon-pencil" />
             <p>Список бажань</p>
           </div>
           <div className="wish-list">
@@ -87,9 +137,12 @@ function ProfileCustomer() {
                   className="wish-list-image"
                 />
                 <div className="wish-list-info">
-                  <h5>{service.name}</h5>
+                  <h6>{service.name}</h6>
                   <p>{service.supplier}</p>
                 </div>
+                <Link to="#" className="remove-wish-list-link">
+                  <img src={minus} alt="Minus" />
+                </Link>
               </div>
             ))}
           </div>
@@ -101,22 +154,81 @@ function ProfileCustomer() {
           </div>
         </div>
 
-        <div className="block">
-          <div className="third-block-header">
-            <img src={pencil} alt="Edit" className="icon-pencil" />
+        <div className="profile-customer-block">
+          <div className="profile-customer-third-block-header">
             <p>Мої замовлення</p>
           </div>
           <div className="my-orders">
-            {orders.map((packageName, index) => (
+            {orders.map((order, index) => (
               <div key={index} className="my-orders-item">
-                {packageName}
+                <img
+                  src={order.image}
+                  alt={order.name}
+                  className="my-orders-image"
+                />
+                <div className="my-orders-info">
+                  <h6>{order.name}</h6>
+                  <p>Дата замовлення: {order.date}</p>
+                  <p>Ціна: {order.price}</p>
+                </div>
+                <Link to="#" className="remove-orders-link">
+                  <img src={minus} alt="Minus" />
+                </Link>
               </div>
             ))}
           </div>
-          <div className="add-service-package">
+          <div className="add-order">
             <img src={plus} alt="Plus" className="plus-my-orders" />
           </div>
         </div>
+      </div>
+
+      <div className="profile-customer-check-letter">
+        <button onClick={handleOpenModal}>Чек-лист</button>
+        {isModalOpen && (
+          <div className="modal-check-letter">
+            <div className="modal-check-letter-content">
+              <img
+                src={greenArrow}
+                alt="Close"
+                className="profile-customer-close-arrow-icon"
+                onClick={handleCloseModal}
+              />
+
+              <h2>Чек-лист</h2>
+              {items.map((item, index) => (
+                <div key={index} className="modal-check-letter-item">
+                  <input
+                    type="checkbox"
+                    checked={item.checked}
+                    onChange={() => handleCheckboxChange(index)}
+                  />
+                  <input
+                    type="text"
+                    value={item.text}
+                    onChange={(e) => handleTextChange(index, e.target.value)}
+                    placeholder="Напишіть тут..."
+                    className={
+                      item.checked && item.text !== "" ? "strikethrough" : ""
+                    }
+                  />
+                  <img
+                    src={greenMinus}
+                    alt="Remove Item"
+                    className="profile-customer-remove-item-button"
+                    onClick={() => handleRemoveItem(index)}
+                  />
+                </div>
+              ))}
+              <img
+                src={greenPlus}
+                alt="Add Item"
+                className="profile-customer-add-item-button"
+                onClick={handleAddItem}
+              />
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
