58620f0eaed80df88bde33b46c1b701faaa6d78f
NureFilatovArtem <artem.filatov@nure.ua>
2024-12-16 01:39:35 +0100

    Update HomeScreen.js

--- mobile/components/HomeScreen.js
+++ mobile/components/HomeScreen.js
@@ -216,30 +216,6 @@ const HomeScreen = () => {
             <Text style={styles.cardPrice}>{service.price} грн</Text>
         </View>
     </TouchableOpacity>
-
-
-
-
-//это твой код, разкоментируй если не хочешь с новым работать, ну он ничего не меняет(пофиксим эту проблему)
-// <TouchableOpacity
-//     key={index}
-//     style={styles.cardContainer}
-//     onPress={() => useNavigation.navigate('OrdersDetailsScreen', {
-//             image: service.image,
-//             title: service.title,
-//             price: service.price,
-//             florist: service.florist || 'Невідомий',
-//             description: service.description || 'Опис цієї послуги недоступний',
-//             rating: service.rating,
-//         })
-//     }
-// >
-//     <Image source={service.image} style={styles.cardImage} />
-//     <View style={styles.textContainer}>
-//         <Text style={styles.cardTitle}>{service.title}</Text>
-//         <Text style={styles.cardPrice}>{service.price} грн</Text>
-//     </View>
-// </TouchableOpacity>
                                     ))}
                             </ScrollView>
                         )}
